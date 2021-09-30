e6ba7aee4f7acd4881399cf378de4c4a82f03774
Valentin Kolb <valentinolb@uni-ulm.de>
2021-09-30 16:26:29 +0200

    modifed setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
-from setuptools import setup
+import setuptools
 
-if __name__ == "__main__":
-    setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='terminal-toolkit',
+    version='0.0.1',
+    author='Valentin Kolb',
+    description="Small library for writing terminal app's",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir="src",
+    url='https://github.com/ValentinKolb/terminal-toolkit',
+    license='MIT',
+    packages=['terminal-toolkit'],
+    python_requires=">=3.9",
+    platforms="unix, linux, osx",
+    install_requires=[
+        'dacite==1.6.0',
+        'webcolors==1.11.1'
+    ]
+)
