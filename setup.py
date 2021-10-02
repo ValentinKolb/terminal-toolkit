17845cb37c9f5670da39768dca90c3a031f3b12b
Your Name <mail@valentin-kolb.com>
2021-10-02 20:46:09 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='terminal',
-    version='0.0.4',
+    version='0.0.5',
     author='Valentin Kolb',
     description="Small library for writing terminal app's",
     long_description=long_description,
